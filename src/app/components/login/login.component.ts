8ae7bf168a8cf562e2e14ec76094a89161437c56
NPERIAS1 <65671+NPERIAS1@users.noreply.github.ford.com>
2024-03-15 11:26:01 +0530

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { CustomerService } from 'src/app/services/customer.service';
+import { LoginCustomer } from 'src/app/models/customer/LoginCustomer';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  loginCustomer:LoginCustomer=new LoginCustomer();
+  constructor(private customerService:CustomerService,private router:Router){}
+  login(){
+  this.customerService.loginCustomer(this.loginCustomer).subscribe(
+    {
+      next:(data) =>{
+        console.log(data);
+          console.log("Login Successful!");
+            alert("Login Successful. Redirecting to Landing Page..")
+            this.router.navigateByUrl("landing/customer");
+      },
+      error:(err) => {
+        console.log(err);
+      },
+      complete:()=>{
+        console.log("completed successfully");
+      }
+    }
+  );
+}
 }
---
+++ src/app/models/customer/LoginCustomer.ts
@@ -0,0 +1,4 @@
+export class LoginCustomer{
+    public customerEmail?:string;
+    public customerPassword?:string;
+}
